05ad0cd4aafc85594e94fa3f4cb386fd9862ff01
paulphilip-louis <paulphilip.louis@gmail.com>
2025-09-21 21:56:40 +0200

    Displaying data

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ import numpy as np
 
 st.header("Visualizing meteorological data in Paris (1991-2024)")
 
-@st.cache_data
+#@st.cache_data
 def load_data():
     weather = pd.read_csv("data/weather.csv")
+    weather = weather.rename(columns={"YEAR":"Year", "MO":"Month", "DY":"Day", "T2M_MAX":"TMax", "T2M_MIN":"TMin"})
     return weather
 
-data_load_state = st.tet("Loading data...")
+data_load_state = st.text("Loading data...")
 weather = load_data()
 data_load_state.text("Loading data... Done !")
 
+st.write(weather)
